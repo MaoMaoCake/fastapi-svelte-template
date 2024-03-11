9a968cafadd5fe5d4f4efc437a02650114bbebcd
Jirapong Pansak (Tang) <tangpansak@gmail.com>
2024-03-11 14:08:18 +0700

    fixed typo

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,15 +1,27 @@
-from pydantic import BaseModel
+"""
+Database CRUD models
+"""
 from typing import Optional
+from pydantic import BaseModel
 
 class CreateEntryResponse(BaseModel):
+    """
+    Return class for creating entries
+    """
     success: bool
-    error = Optional[str]
+    error: Optional[str]
     created_id: int
 
 class UpdateEntryResponse(BaseModel):
+    """
+    Return class for Updating entries
+    """
     success: bool
     error: Optional[str]
 
 class DeleteEntryResponse(BaseModel):
+    """
+    Return class for Deleting entries
+    """
     success: bool
     error : Optional[str]
